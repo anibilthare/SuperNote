33e6c86901ffe435a607405a2b16e55dc05516dd
Mayank Mathur <mynk-9@users.noreply.github.com>
2019-12-24 17:58:28 +0530

    Important relocation.

---
+++ db_manager.js
@@ -0,0 +1,19 @@
+var database = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+database.connect(url, function(err, db) {
+	if (err)
+		throw err;
+	var dbo = db.db("SuperNote");
+	var obj = {
+		id: "ipdipode",
+		time: "7:24 pm",
+		note: "sdvaskjfhajfnjasdnfkjsdnfjkdnfljdsngsbd"
+	};
+	dbo.collection("Notes").insertOne(obj, function(err, res) {
+		if (err)
+			throw err;
+		console.log("1 doc inserted!");
+		db.close();
+	});
+});
