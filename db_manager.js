ab878dcc5cce7928483ed0cf10e76242a887ba8b
Mayank Mathur <mynk-9@users.noreply.github.com>
2019-12-25 23:29:09 +0530

    Update db_manager.js

--- db_manager.js
+++ db_manager.js
@@ -1,13 +1,43 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host: "",
-	user: "",
-	password: ""
+	host: "localhost",
+	user: "root",
+	password: "root"
 });
 
 connection.connect(function(err) {
 	if (err)
 		throw err;
-	console.log("connection established.");
+    console.log("connection established.");
+
+    var sqlQ = "CREATE DATABASE IF NOT EXIST notes;";    
+    connection.query(sqlQ, function (err, result) {
+        if (err)
+            throw err;
+        console.log("Init-1");
+    });
+    
+    var sqlQ = "USE notes;";    
+    connection.query(sqlQ, function (err, result) {
+        if (err)
+            throw err;
+        console.log("Init-2");
+    });
+
+    sqlQ = "CREATE TABLE IF NOT EXISTS test (title TINYTEXT, data TEXT, creation TIMESTAMP, modified TIMESTAMP);";
+    connection.query(sqlQ, function (err, result) {
+        if (err)
+            throw err;
+        console.log("Init-3");
+    }); 
 });
+
+function executeSQLQuery (sql, consoleLogMessage) {
+    connection.query(sql, function(err, result){
+        if (err)
+            throw err;
+        if (consoleLogMessage != "")
+            console.log(consoleLogMessage);
+    });
+}
