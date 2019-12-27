8ee2e3a86e9a70333cbcd514411ab39eafc12068
Mayank Mathur <mynk-9@users.noreply.github.com>
2019-12-28 02:07:44 +0530

    Further changes.
    Struck at a callback code in login prototype function etc. of database.js export.

--- abc.js
+++ abc.js
@@ -2,7 +2,9 @@ var abc = require("./database");
 
 abc.set_connection("localhost", "root", "root");
 abc.initialise();
-abc.create_user("mayank", "pass123", "Mayank Mathur");
-abc.login("mayank", "pass123");
-abc.newNote("mayank", "pass123", "hello mera nam mayank hai.", "xyz", "CURTIME()");
+
+//abc.create_user("mayank", "pass123", "Mayank Mathur");
+if (abc.login("mayank", "pass123") == true) console.log("LOGIN SUCCESS!");
+//abc.newNote("mayank", "pass123", "hello mera nam mayank hai.", "xyz", "CURTIME()");
+
 abc.end_connection();
--- database.js
+++ database.js
@@ -5,9 +5,12 @@ module.exports = {
 
     /// properties_and_internal_functions {
 
-    CONNECTION_SET: false,
-    USER_TABLE_STR: "username VARCHAR(50) PRIMARY KEY, password VARCHAR(25), name VARCHAR(50)",
-    NOTE_TABLE_STR: "title varchar(100), note TEXT, creation TIMESTAMP PRIMARY KEY, modified TIMESTAMP",
+    __CONNECTION_SET: false,
+    __USER_TABLE_STR: "username VARCHAR(50) PRIMARY KEY, password VARCHAR(25), name VARCHAR(50)",
+    __NOTE_TABLE_STR: "title varchar(100), note TEXT, creation TIMESTAMP PRIMARY KEY, modified TIMESTAMP",
+    __LOG_MESSAGE: "database API > ",
+    __TRIP: [0,0,0,0],
+
     
     __executeSQLQuery: function (sql, consoleLogMessage) { //!// internal usage only
         this.connection.query(sql, function(err, result) {
@@ -28,12 +31,12 @@ module.exports = {
             user: _user,
             password: _password
         });
-        this.CONNECTION_SET = true;
+        this.__CONNECTION_SET = true;
     },
 
     initialise: function () {
-        if (!this.CONNECTION_SET) {
-            console.log("Please set up connection first.");
+        if (!this.__CONNECTION_SET) {
+            console.log(this.__LOG_MESSAGE + "Please set up connection first.");
             return;
         }
         this.connection.connect(function(err) {
@@ -49,49 +52,58 @@ module.exports = {
         sqlQ = "USE supernote;";
         this.__executeSQLQuery(sqlQ, "Init-2");
 
-        sqlQ = "CREATE TABLE IF NOT EXISTS users (" + this.USER_TABLE_STR + ");";
+        sqlQ = "CREATE TABLE IF NOT EXISTS users (" + this.__USER_TABLE_STR + ");";
         this.__executeSQLQuery(sqlQ, "Init-3");
         /// }
     },
 
     check_username: function (username) {
-        _username = this.connection.escape(username);
+        var _username = this.connection.escape(username);
+        var check = false;
+
         this.connection.query("SELECT * FROM users WHERE username = " + _username + ";",
             function(err, result, fields) {
                 if (result.length !== 0) { /// !== because type safe check required here
-                    return true;
+                    console.log("database API > " + "USERNAME ALREADY EXISTS.");
+                    check = true;
                 }
             }
         );
-        return false;
+        console.log(this.__LOG_MESSAGE + check);
+        return check;
     },
 
     create_user: function (username, pass, name) {
-        if (this.check_username(username))
+        if (this.check_username(username) == true) {
+            console.log(this.__LOG_MESSAGE + "NEW USER REJECTED.");
             return false;
+        }
 
-        _username = this.connection.escape(username);
-        _pass = this.connection.escape(pass);
-        _name = this.connection.escape(name);
+        var _username = this.connection.escape(username);
+        var _pass = this.connection.escape(pass);
+        var _name = this.connection.escape(name);
 
         var sqlQ = "INSERT INTO users VALUES (" + _username + ", " + _pass + ", " + _name + ");";
-        this.__executeSQLQuery(sqlQ, "NEW USR CREATED-1");
+        this.__executeSQLQuery(sqlQ, "NEW USER CREATED-1");
 
-        sqlQ = "CREATE TABLE " + _username.substring(1, _username.length - 1) + " (" + this.NOTE_TABLE_STR + ");"; /// escaped string comes with single quotes
-        this.__executeSQLQuery(sqlQ, "NEW USR CREATED-2");
+        sqlQ = "CREATE TABLE " + _username.substring(1, _username.length - 1) + " (" + this.__NOTE_TABLE_STR + ");"; /// escaped string comes with single quotes
+        this.__executeSQLQuery(sqlQ, "NEW USER CREATED-2");
 
         return true;
     },
 
     login: function (username, pass) {
-        _username = this.connection.escape(username);
-        _pass = this.connection.escape(pass);
+        var _username = this.connection.escape(username);
+        var _pass = this.connection.escape(pass);
         var success = false;
+
+        function toggleSuccess() {success = !success};
 
         this.connection.query("SELECT password FROM users WHERE username = " + _username + ";",
             function(err, result, fields) {
-                if (result[0].password == _pass) {
-                    success = true;
+                if (result[0].password == _pass.substring(1, _pass.length - 1)) {
+                    toggleSuccess();
+                    console.log(result);
                 }
             }
         );
@@ -100,11 +112,11 @@ module.exports = {
     },
 
     newNote: function (username, pass, note, title, timestamp) {
-        _username = this.connection.escape(username);
-        _pass = this.connection.escape(pass);
-        _note = this.connection.escape(note);
-        _title = this.connection.escape(title);
-        _timestamp = this.connection.escape(timestamp);
+        var _username = this.connection.escape(username);
+        var _pass = this.connection.escape(pass);
+        var _note = this.connection.escape(note);
+        var _title = this.connection.escape(title);
+        var _timestamp = this.connection.escape(timestamp);
 
         if (this.login(username, pass)) {
             var sqlQ = "INSERT INTO " + _username + " values (" + _title + ", " 
@@ -112,7 +124,6 @@ module.exports = {
             this.__executeSQLQuery(sqlQ, "NEW NOTE ADDED");
         }
     },
-
 
     end_connection: function() {
         this.connection.end();
